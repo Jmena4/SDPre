34ca48fd3276230880dd5644f19a7fb5be3bc9c3
Jesse <Jesse.mena4@gmail.com>
2018-12-21 19:22:39 -0500

    add ForgetfulMachine.java

--- ForgetfulMachine.java
+++ ForgetfulMachine.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 public class ForgetfulMachine {
 	public static void main( String[] args ){
 		Scanner keyboard = new Scanner(System.in);
-		
+
 		System.out.println( "what city is the capital of France?" );
 		keyboard.next();
-		
+
 		System.out.println( "What is 6 multiplied by 7?" );
 		keyboard.nextInt();
 		//program blows up when you type words
