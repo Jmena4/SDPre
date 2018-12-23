d78dba3f5be638554eb0b5eb47fea4e65cd6f2ef
Jmena4 <45954897+Jmena4@users.noreply.github.com>
2018-12-22 21:20:55 -0500

    Delete FirstProg.java

--- FirstProg.java
+++
@@ -1,6 +0,0 @@
-public class FirstProg {
-	public static void main (String [] args) {
-		System.out.println( "I, Jesse Mena, am determined to learn how to code.");
-		System.out.println( "Today’s date is Monday, December 17, 2019." );
-}
-}
