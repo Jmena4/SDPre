026cae72fd0768a15933c216714bc1abc48aa89d
Jesse <Jesse.mena4@gmail.com>
2018-12-23 11:33:15 -0500

    add Objective6Lab2.java

---
+++ Labs2/Objective6Lab2.java
@@ -0,0 +1,11 @@
+public class Objective6Lab2 {
+    public static void main(String[] args ){
+        int counter = 0 ;
+
+        counter = counter++;
+        while(counter <= 20) {
+            System.out.println(counter++);
+        }
+
+    }
+}
