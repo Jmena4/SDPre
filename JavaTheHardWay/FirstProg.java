a6c9609f897d57542f0906b2ff7b310ec6de424b
Jesse <Jesse.mena4@gmail.com>
2018-12-22 21:08:17 -0500

    add JavaTheHardWay folder

---
+++ JavaTheHardWay/FirstProg.java
@@ -0,0 +1,6 @@
+public class FirstProg {
+	public static void main (String [] args) {
+		System.out.println( "I, Jesse Mena, am determined to learn how to code.");
+		System.out.println( "Today’s date is Monday, December 17, 2019." );
+}
+}
